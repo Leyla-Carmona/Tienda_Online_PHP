383c869c63987236f41f2da73c3fb3aa7bbfb622
Leyla Carmona <leylacarmona2010@hotmail.es>
2024-11-27 07:53:59 -0600

    $git commit

--- conexion.php
+++ conexion.php
@@ -1,17 +1,16 @@
 <?php
-require 'vendor/autoload.php';
+// Variables de conexión a la base de datos
+$host = "aws-0-us-west-1.pooler.supabase.com";
+$usuario = "postgres.fbkljamdhlvfrwjguezi";
+$contraseña = "_s@ntT5BYEMWzi-";
+$nombreBaseDatos = "postgres";
+$puerto = "6543"; // Asegúrate de usar el puerto correcto
 
-$url = 'https://fbkljamdhlvfrwjguezi.supabase.co';
-$apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZia2xqYW1kaGx2ZnJ3amd1ZXppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3MTIxMDYsImV4cCI6MjA0ODI4ODEwNn0.xNPK1LgBttlrasJcbTL8Z1_qblQktM7yvSjL8cevRpg';
-
-$client = new \Supabase\SupabaseClient($url, $apiKey);
-
-// Consulta ejemplo
-$response = $client->from('productos')->select('codigo, nombre, detalle, imagen')->gte('codigo', 10)->execute();
-
-// Verifica el resultado
-if ($response->error) {
-    echo 'Error: ' . $response->error->message;
-} else {
-    print_r($response->data);
+// Crear la conexión con PDO
+try {
+    $conn = new PDO("pgsql:host=$host;port=$puerto;dbname=$nombreBaseDatos", $usuario, $contraseña);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa";
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
 }
