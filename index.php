dfce88608bb5086fa6921bb37967800f653fae36
Leyla Carmona <leylacarmona2010@hotmail.es>
2024-11-27 20:47:27 -0600

    $git commit

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ $usuario = getenv('DB_USER');
 $contraseña = getenv('DB_PASSWORD');
 $nombreBaseDatos = getenv('DB_NAME');
 $puerto = getenv('DB_PORT');
+$port = getenv('PORT') ?: 80; // Usa la variable de entorno PORT o el puerto 80 como predeterminado
+echo "Servidor escuchando en el puerto: $port\n";
+exec("php -S 0.0.0.0:$port");
 
 try {
     $conn = new PDO("pgsql:host=$host;port=$puerto;dbname=$nombreBaseDatos", $usuario, $contraseña);
