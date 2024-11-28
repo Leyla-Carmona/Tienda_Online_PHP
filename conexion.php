446a0c8260bf7c2327fa4990ffcd15f3aaed3413
Leyla Carmona <leylacarmona2010@hotmail.es>
2024-11-27 20:24:26 -0600

    $git commit

--- conexion.php
+++ conexion.php
@@ -6,7 +6,6 @@ $contraseña = getenv('DB_PASSWORD');
 $nombreBaseDatos = getenv('DB_NAME');
 $puerto = getenv('DB_PORT');
 
-// Crear la conexión con PDO
 try {
     $conn = new PDO("pgsql:host=$host;port=$puerto;dbname=$nombreBaseDatos", $usuario, $contraseña);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
