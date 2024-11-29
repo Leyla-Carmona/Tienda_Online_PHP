41d7035ce3673f5796250955c4bd9c64dd3124c6
Leyla Carmona <leylacarmona2010@hotmail.es>
2024-11-28 23:56:14 -0600

    $git commit

--- conexion.php
+++ conexion.php
@@ -5,10 +5,11 @@ $usuario = getenv('DB_USER');
 $contraseña = getenv('DB_PASSWORD');
 $nombreBaseDatos = getenv('DB_NAME');
 $puerto = getenv('DB_PORT');
-$port = getenv('APP_PORT') ?: 8080; // Usa la variable de entorno APP_PORT o 8080 como respaldo
-$host = "0.0.0.0"; // Escuchar en todas las interfaces de red
+$port = getenv('APP_PORT') ?: 8080; // Usa la variable de entorno APP_PORT, o 8080 como respaldo si no está definida
+$host = "0.0.0.0"; // Escucha en todas las interfaces de red
 
-// Iniciar el servidor PHP
+// Inicia el servidor PHP
+echo "Iniciando servidor en http://$host:$port\n";
 exec("php -S $host:$port");
 
 try {
